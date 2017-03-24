39ecccc4bc07fcf14bff59e722e03455bd180972
Masato Komatsubara <masa@Masato-no-MacBook.local>
2017-03-24 15:14:38 +0900

    add group creating funtion

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,22 @@
 class GroupsController < ApplicationController
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    Group.create(create_params)
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  private
+  def create_params
+    params.require('group').permit(:name, user_ids:[])
   end
 end
