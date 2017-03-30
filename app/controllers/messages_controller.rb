47c87eb19c4df751feb546b072d1cc1ba1ad52e5
Masato Komatsubara <masa@Masato-no-MacBook.local>
2017-03-30 15:37:57 +0900

    add message sending function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,11 +2,20 @@ class MessagesController < ApplicationController
   def index
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
-    @messages = Message.all
+    @messages = Message.where(group_id: params[:group_id], user_id: current_user.id)
     @message = Message.new
   end
 
   def create
-    redirect_to root_path
+    Message.create(message_params)
+    redirect_to :back
+  end
+
+  def update
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(params.permit(:group_id)).merge(user_id: current_user.id)
   end
 end
