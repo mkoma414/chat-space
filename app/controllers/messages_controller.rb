c46e975807ea3d3b4f1a199a190a9fb6c1607ba8
Masato Komatsubara <masa@Masato-no-MacBook.local>
2017-03-30 14:42:02 +0900

    fix layout

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,5 +3,10 @@ class MessagesController < ApplicationController
     @groups = current_user.groups
     @group = Group.find(params[:group_id])
     @messages = Message.all
+    @message = Message.new
+  end
+
+  def create
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "groups#index"
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:index]
+    resources :messages #, only: [:index, :create]
   end
 end
