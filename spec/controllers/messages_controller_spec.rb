98b4e1d18d0a07071c6b3c467afe5a30a33ecc65
Masato Komatsubara <masa@Masato-no-MacBook.local>
2017-04-04 20:51:38 +0900

    fix spec file

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,24 +1,22 @@
 require 'rails_helper'
 
 describe MessagesController do
+  let(:user){create(:user)}
+  before do
+    sign_in user
+  end
   describe 'GET #index' do
     it "assigns the requested grouops to @groups" do
-      user = create(:user)
-      sign_in user
       get :index, params: { group_id: user.groups[0].id }
       expect(assigns(:groups)).to eq user.groups
     end
 
     it "assigns the requested current_grouops to @current_groups" do
-      user = create(:user)
-      sign_in user
       get :index, params: { group_id: user.groups[0].id }
       expect(assigns(:current_group)).to eq user.groups[0]
     end
 
     it "renders the :index template" do
-      user = create(:user)
-      sign_in user
       get :index, params: { group_id: user.groups[0].id }
       expect(response).to render_template :index
     end
@@ -26,15 +24,11 @@ describe MessagesController do
 
   describe 'POST #create' do
     it "assigns the requested message to message" do
-      user = create(:user)
-      sign_in user
       post :create, group_id: user.groups[0].id, params: {message: {body: user.groups[0].messages[0].body}, group_id: user.groups[0].id }
       expect(assigns(:message).body).to eq user.groups[0].messages[0].body
     end
 
     it "renders the :create template" do
-      user = create(:user)
-      sign_in user
       post :create, group_id: user.groups[0].id, params: {message: {body: user.groups[0].messages[0].body}, group_id: user.groups[0].id }
       expect(response).to render_template :create
     end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,11 +3,14 @@ require 'rails_helper'
 FactoryGirl.define do
 
   factory :user do
-    name                  "komatsubara"
-    email                 "m-koma@nifty.com"
-    password              "password"
-    password_confirmation "password"
-    confirmed_at          "2017-03-24 16:30:30"
+    password = Faker::Internet.password(min_length = 8)
+
+    name                  Faker::Name.name
+    email                 Faker::Internet.email
+    password              password
+    password_confirmation password
+    # confirmed_at          "2017-03-24 16:30:30"
+    confirmed_at          DateTime.now
 
     after(:create) do |user|
       user.groups << create(:group)
